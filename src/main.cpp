6c75d8a5de7bcb3b0306678cf7def2650ec6a9c5
Marcel Rød <marcelroed@gmail.com>
2019-08-12 21:46:24 +0200

    Changes to potentials for the different modes

--- src/main.cpp
+++ src/main.cpp
@@ -32,30 +32,29 @@ void initializeSimulationMode(int newMode, PotentialFlow& simulator, ShapeRender
         case RANKINES_OVAL:{
             float distance = 300.0f;
             simulator.addPotential(uniform(90.0f, sf::Vector2f(1.0f, 0.0f)));
-            simulator.addPotential(source(6000.0f, sf::Vector2f(width/2 - distance/2, height/2)));
-            simulator.addPotential(source(-6000.0f, sf::Vector2f(width/2 + distance/2, height/2)));
-            simulator.addParticlePointSource(.3f, 11, sf::Vector2f(width/2 - distance/2, height/2));
-            simulator.addParticlePointDrain(20.0f, sf::Vector2f(width/2 + distance/2, height/2));
+            simulator.addPotential(source(6000.0f, sf::Vector2f(width/2.0f - distance/2, height/2.0f)));
+            simulator.addPotential(source(-6000.0f, sf::Vector2f(width/2.0f + distance/2, height/2.0f)));
+            simulator.addParticlePointSource(.3f, 11, sf::Vector2f(width/2.0f - distance/2, height/2.0f));
+            simulator.addParticlePointDrain(20.0f, sf::Vector2f(width/2.0f + distance/2, height/2.0f));
             simulator.addParticleLineSource(.3f, 41, sf::Vector2f(-5,0), sf::Vector2f(-5, height));
             break;
         }
         case OUTWARD_WHIRL:
-            simulator.addPotential(source(12000.0f, sf::Vector2f(width/2, height/2)));
-            simulator.addParticlePointSource(.1f, 7, sf::Vector2f(width/2, height/2));
-            simulator.addPotential(whirl(24000.0f, sf::Vector2f(width/2, height/2)));
+            simulator.addPotential(source(12000.0f, sf::Vector2f(width/2.0f, height/2.0f)));
+            simulator.addParticlePointSource(.1f, 7, sf::Vector2f(width/2.0f, height/2.0f));
+            simulator.addPotential(whirl(24000.0f, sf::Vector2f(width/2.0f, height/2.0f)));
             break;
         /*case MAGNUS: {
-
             break;
         }*/
         case CIRCLE: {
             float radius = 150.0f;
-            sf::CircleShape* circleP = new sf::CircleShape(radius);
+            auto* circleP = new sf::CircleShape(radius);
             circleP->setFillColor(sf::Color(255,0,0));
-            circleP->setPosition(sf::Vector2f(width/2 - radius, height/2 - radius));
+            circleP->setPosition(sf::Vector2f(width/2.0f - radius, height/2.0f - radius));
             shapeRenderer.addShape(circleP);
             simulator.addPotential(uniform(90.0, sf::Vector2f(1.0f, 0.0f)));
-            simulator.addPotential(doublet(2400000.0, sf::Vector2f(width / 2, height / 2)));
+            simulator.addPotential(doublet(2400000.0, sf::Vector2f(width / 2.0f, height / 2.0f)));
             //simulator.addParticlePointSource(0.3f, 10, sf::Vector2f(width/2, height/2));
             simulator.addParticleLineSource(0.3f, 41, sf::Vector2f(0.0f, 0.0f), sf::Vector2f(0.0f, height));
             //simulator.addPotential(whirl(12000.0, sf::Vector2f(width/2, height/2)));
@@ -63,14 +62,14 @@ void initializeSimulationMode(int newMode, PotentialFlow& simulator, ShapeRender
             break;
         }
         case SPECIAL:
-            simulator.specialParticles = std::vector<int>({1008, 1029, 1050, 1113, 1156, 1176, 1238, 1240, 1283, 1303, 1346, 1349, 1361, 1363, 1365, 1366, 1388, 1410, 1413, 1422, 1423, 1424, 1426, 1427, 1430, 1473, 1476, 1478, 1483, 1486, 1488, 1489, 1493, 1537, 1540, 1549, 1551, 1555, 1556, 1578, 1600, 1603, 1611, 1620, 1666, 1667, 1673, 1729, 1730, 1735, 1736, 1745, 1746, 1764, 1768, 1785, 1793, 1794, 1795, 1796, 1797, 1806, 1810, 1868, 1869, 1912, 1930, 1931, 1936, 1937, 1976, 1992, 1993, 1998, 2017, 2039, 2041, 2042, 2053, 2055, 2059, 2085, 2104, 2105, 2106, 2116, 2118, 2122, 2126, 2127, 2142, 2146, 2163, 2164, 2165, 2166, 2171, 2172, 2174, 2178, 2179, 2181, 2188, 2205, 2207, 2226, 2227, 2228, 2229, 2231, 2237, 2244, 2247, 2254, 2268, 2270, 2273, 2289, 2294, 2300, 2303, 2304, 2306, 2310, 2312, 2316, 2331, 2336, 2337, 2352, 2357, 2358, 2363, 2367, 2369, 2373, 2374, 2375, 2378, 2394, 2395, 2400, 2402, 2415, 2420, 2421, 2422, 2423, 2426, 2428, 2430, 2432, 2437, 2438, 2443, 2458, 2460, 2464, 2481, 2483, 2487, 2489, 2491, 2493, 2495, 2498, 2500, 2501, 2521, 2523, 2527, 2529, 2542, 2544, 2552, 2553, 2556, 2558, 2564, 2565, 2571, 2584, 2586, 2591, 2605, 2606, 2607, 2615, 2616, 2619, 2621, 2622, 2623, 2627, 2628, 2633, 2649, 2655, 2656, 2669, 2670, 2673, 2674, 2678, 2682, 2684, 2685, 2691, 2698, 2713, 2719, 2736, 2737, 2738, 2739, 2741, 2776, 2782, 2783, 2825, 2839, 2843, 2846, 2907, 2949, 2952, 2971, 3012, 3013, 3035, 3075, 3078, 3099, 3100, 3140, 3163, 3164, 3205, 3226, 3227, 3265, 3267, 3269, 3288, 3289, 3351, 3392, 3394, 3396, 3414, 3457, 3459, 3477, 3519, 3521, 3522, 3540, 3541, 3542, 3543, 3544, 3585});
+            simulator.specialParticles = std::vector<int>({1037, 1052, 1083, 1114, 1143, 1144, 1176, 1204, 1237, 1264, 1265, 1282, 1299, 1325, 1327, 1328, 1360, 1361, 1364, 1382, 1385, 1387, 1388, 1389, 1422, 1426, 1427, 1440, 1441, 1442, 1443, 1446, 1447, 1450, 1484, 1488, 1489, 1491, 1492, 1497, 1498, 1502, 1503, 1506, 1507, 1508, 1545, 1546, 1549, 1550, 1551, 1562, 1563, 1566, 1567, 1573, 1607, 1611, 1612, 1623, 1627, 1634, 1669, 1672, 1673, 1683, 1734, 1735, 1742, 1743, 1756, 1769, 1772, 1784, 1796, 1797, 1798, 1799, 1800, 1801, 1802, 1815, 1818, 1830, 1845, 1846, 1874, 1875, 1876, 1908, 1934, 1935, 1939, 1970, 1994, 1995, 2001, 2017, 2031, 2034, 2035, 2052, 2055, 2063, 2075, 2092, 2093, 2095, 2096, 2097, 2098, 2099, 2100, 2112, 2115, 2116, 2121, 2135, 2136, 2138, 2150, 2151, 2152, 2153, 2154, 2157, 2161, 2162, 2163, 2166, 2167, 2172, 2173, 2176, 2182, 2184, 2196, 2201, 2211, 2212, 2213, 2214, 2215, 2218, 2227, 2228, 2232, 2234, 2237, 2242, 2246, 2247, 2257, 2272, 2279, 2288, 2292, 2293, 2295, 2298, 2303, 2318, 2322, 2333, 2340, 2341, 2342, 2349, 2353, 2355, 2356, 2358, 2359, 2364, 2365, 2379, 2381, 2385, 2394, 2402, 2403, 2404, 2405, 2406, 2407, 2410, 2413, 2414, 2416, 2419, 2424, 2426, 2429, 2430, 2431, 2442, 2445, 2456, 2460, 2463, 2471, 2473, 2474, 2477, 2480, 2484, 2485, 2487, 2488, 2502, 2506, 2517, 2518, 2521, 2524, 2532, 2534, 2538, 2541, 2544, 2545, 2548, 2549, 2563, 2564, 2568, 2569, 2579, 2580, 2581, 2582, 2585, 2593, 2594, 2595, 2599, 2602, 2603, 2604, 2605, 2610, 2615, 2629, 2630, 2641, 2646, 2647, 2648, 2649, 2650, 2651, 2654, 2655, 2660, 2662, 2663, 2664, 2671, 2687, 2691, 2709, 2710, 2711, 2712, 2715, 2721, 2748, 2799, 2809, 2812, 2858, 2873, 2874, 2919, 2935, 2981, 2997, 3059, 3104, 3105, 3121, 3164, 3181, 3228, 3242, 3288, 3303, 3348, 3349, 3363, 3409, 3412, 3425, 3472, 3473, 3486, 3487, 3488});
             std::sort(simulator.specialParticles.rbegin(), simulator.specialParticles.rend());
 
-            simulator.addPotential(source(-35000, sf::Vector2f(width/2, height)));
-            simulator.addParticlePointDrain(20.0, sf::Vector2f(width/2, height));
-            simulator.addParticleLineSource(0.4, 21, sf::Vector2f(0,0), sf::Vector2f(0, height));
-            simulator.addParticleLineSource(0.4, 21, sf::Vector2f(0,0), sf::Vector2f(width, 0));
-            simulator.addParticleLineSource(0.4, 21, sf::Vector2f(width,0), sf::Vector2f(width, height));
+            simulator.addPotential(source(-35000, sf::Vector2f(width/2.0f, height)));
+            simulator.addParticlePointDrain(20.0, sf::Vector2f(width/2.0f, height));
+            simulator.addParticleLineSource(0.4, 15, sf::Vector2f(0,0), sf::Vector2f(0, height));
+            simulator.addParticleLineSource(0.4, 31, sf::Vector2f(0,0), sf::Vector2f(width, 0));
+            simulator.addParticleLineSource(0.4, 15, sf::Vector2f(width,0), sf::Vector2f(width, height));
             break;
         default:
             break;
@@ -113,7 +112,7 @@ void potentialFlow(){
             fs.particles.push_back(p);
         }
     }*/
-    printf("Particle count: %i\n", fs.particles.size());
+    printf("Particle count: %i\n", (int)fs.particles.size());
     // Run while window is open
     while (window.isOpen())
     {
